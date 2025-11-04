f8e9c63c853f6bfacadc049c2731244e89d384cb
Abhimanyu Sharma <56254966+Abhisharma08@users.noreply.github.com>
2025-11-04 15:38:01 +0530

    logo fixed

--- src/components/haeywa/Logo.tsx
+++ src/components/haeywa/Logo.tsx
@@ -7,8 +7,8 @@ const Logo = ({ className }: { className?: string }) => {
       <Image
         src="https://haeywa.ai/img/haeywa_logo.webp"
         alt="haeywa Logo"
-        width={120}
-        height={40}
+        width={80}
+        height={20}
         className="object-contain w-[100px] sm:w-[120px] md:w-[140px] h-auto"
         priority
       />
